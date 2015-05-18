ef9a654a12da58cfea984b783af5db67b9aa9472
aleszp <aleksander.szsz@gmail.com>
2015-05-18 20:39:58 +0200

    Dodano wstępną wersję wykresów JFreeChart

--- src/pl/pw/edu/fizyka/pojava/formatC/tranzystor/Graph.java
+++ src/pl/pw/edu/fizyka/pojava/formatC/tranzystor/Graph.java
@@ -1,18 +1,60 @@
 package pl.pw.edu.fizyka.pojava.formatC.tranzystor;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
-public class Graph extends JPanel {
-	
-	// TODO Doko�czy� u�ywaj�c wybranego elemty do budowy grafu
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
+/**
+ *Displays calculated values.  
+ *@author Aleksander Szpakiewicz-Szatan
+ */
+public class Graph extends JPanel 
+{
 	private static final long serialVersionUID = 1L;
+	
+	XYSeries dataSeries;
+	XYSeriesCollection dataCollection;
+	int voltageId;
+	int currentId;
 
-	public Graph(Color frameColor) 
+	public Graph(Color frameColor, int voltageId_, int currentId_) 
 	{
 		setBorder(new LineBorder(frameColor));
+		voltageId=voltageId_;
+		currentId=currentId_;
+			
+		dataSeries =new XYSeries("");
+		dataCollection = new XYSeriesCollection();
+		dataCollection.addSeries(dataSeries);
+		
+		JFreeChart chart = ChartFactory.createXYLineChart
+				(
+				"Natężenie prądu",
+				"Napięcie",
+				"Prąd",
+				dataCollection, 
+				PlotOrientation.VERTICAL, 
+				false, 
+				true,
+				false);
+		final ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setMinimumSize(new java.awt.Dimension(320, 240));
+		chartPanel.setPreferredSize(new java.awt.Dimension(640, 480));
+		chartPanel.setMaximumSize(new java.awt.Dimension(1280, 1024));
+		setLayout(new GridLayout());
+		this.add(chartPanel);
+	}
+	void addData(double x, double y)
+	{
+		dataSeries.add(x,y);
 	}
 }
--- src/pl/pw/edu/fizyka/pojava/formatC/tranzystor/GraphsPanel.java
+++ src/pl/pw/edu/fizyka/pojava/formatC/tranzystor/GraphsPanel.java
@@ -18,10 +18,21 @@ public class GraphsPanel extends JPanel
 		layot.setVgap(10);
 		layot.setHgap(10);
 		
-		graph1=new Graph(frameColor);
+		graph1=new Graph(frameColor,0,0);
 		add(graph1);
-		graph2=new Graph(frameColor);
+		graph2=new Graph(frameColor,0,0);
 		add(graph2);
+		
+		//TEST
+		graph1.addData(1,1);
+		graph1.addData(2,2);
+		graph1.addData(3,1);
+		graph1.addData(4,5);
+		graph1.addData(6,7);
+		
+		for(double ii=0.0;ii<10;ii+=0.1)
+			graph2.addData(ii,ii*ii-7*ii+5);
+				
+		
 	}
-
 }
